5a8e6d5a2e2a2f9e3e49de1191c71b43f60efd3e
LoreOth <72841422+LoreOth@users.noreply.github.com>
2021-06-16 22:47:53 -0300

    Update CompareNumbers.java
    Método que compara dos números y devuelve el máximo.-

--- src/main/java/com/redbee/academy/challenge/CompareNumbers.java
+++ src/main/java/com/redbee/academy/challenge/CompareNumbers.java
@@ -1,18 +1,33 @@
-package com.redbee.academy.challenge;
 
 public class CompareNumbers {
 
-  /**
-   * Method that receives two numbers and returns the
-   * max of both
-   *
-   * @param a - Some Integer Number
-   * @param b - Another Integer Number
-   * @param c - Another Integer Number
-   * @return The max of a, b and c
-   */
-  public static Integer max(Integer a, Integer b, Integer c) {
-    //TODO: Implement me
-    return null;
-  }
+	/**
+	 * Method that receives two numbers and returns the max of both
+	 *
+	 * @param a - Some Integer Number
+	 * @param b - Another Integer Number
+	 * @param c - Another Integer Number
+	 * @return The max of a, b and c
+	 */
+	public static Integer max(Integer a, Integer b, Integer c) {
+
+		if (a > b) {
+			if (a > c) {
+				return a;
+			} else
+				return c;
+		}
+		if (b > c) {
+			return b;
+		} else {
+			return c;
+		}
+	}
+
+	public static void main(String[] args) {
+
+		System.out.println(CompareNumbers.max(59, 59, 5));
+
+	}
+
 }
