7292c8f024321776b86c88c468d2088085a5d3c1
LoreOth <72841422+LoreOth@users.noreply.github.com>
2021-06-16 22:46:42 -0300

    Update CompareArrayNumbers.java
    Método estático que almacena en una lista de Integer, el máximo entre los elementos de dos listas.-

--- src/main/java/com/redbee/academy/challenge/CompareArrayNumbers.java
+++ src/main/java/com/redbee/academy/challenge/CompareArrayNumbers.java
@@ -1,22 +1,35 @@
-package com.redbee.academy.challenge;
 
+//import com.sun.tools.javac.util.List;
 import java.util.List;
+import java.util.ArrayList;
+import java.util.Iterator;
 
 public class CompareArrayNumbers {
 
-  /**
-   * Method that receives two lists and returns
-   * another list with de max of each index
-   *
-   * @param a - Some Integer Number
-   * @param b - Another Integer Number
-   * @return The result of a + b
-   */
-  public static List<Integer> max(List<Integer> a, List<Integer> b) {
-    //TODO: Implement me
-    return null;
-  }
+	/**
+	 * Method that receives two lists and returns another list with de max of each
+	 * index
+	 *
+	 * @param a - Some Integer Number
+	 * @param b - Another Integer Number
+	 * @return The result of a + b
+	 */
+	public static List<Integer> max(List<Integer> a, List<Integer> b) {
+		List<Integer> maximo;
+		maximo = new ArrayList<>();
 
+		Iterator<Integer> ita = a.iterator();
+		Iterator<Integer> itb = b.iterator();
+		while ((ita.hasNext() && (itb.hasNext()))) {
+			maximo.add(Math.max(ita.next(), itb.next()));
+		}
+		if (ita.hasNext()) {
+			while (ita.hasNext()) {
+				maximo.add(ita.next());
+			}
+		} else {
+			while (itb.hasNext()) {
+				maximo.add(itb.next());
+			}
 
-
-}
+		}
