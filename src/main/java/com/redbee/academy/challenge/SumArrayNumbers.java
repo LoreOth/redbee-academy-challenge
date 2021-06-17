d0f131d03c00e166451d62eb5ed5bc7b4b3d77bf
LoreOth <72841422+LoreOth@users.noreply.github.com>
2021-06-16 22:48:37 -0300

    Update SumArrayNumbers.java
    Método que suma los elementos de una lista.-

--- src/main/java/com/redbee/academy/challenge/SumArrayNumbers.java
+++ src/main/java/com/redbee/academy/challenge/SumArrayNumbers.java
@@ -1,18 +1,36 @@
-package com.redbee.academy.challenge;
-
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class SumArrayNumbers {
 
-  /**
-   * Method that receives an array of numbers and
-   * returns the sum of each number
-   *
-   * @param array - Some integer list. ([1,2,3])
-   * @return The result of 1 + 2 + 3
-   */
-  public static Integer sum(List<Integer> array) {
-    //TODO: Implement me
-    return null;
-  }
+	/**
+	 * Method that receives an array of numbers and returns the sum of each number
+	 *
+	 * @param array - Some integer list. ([1,2,3])
+	 * @return The result of 1 + 2 + 3
+	 */
+	public static Integer sum(List<Integer> array) {
+		Integer total = 0;
+		Iterator<Integer> it = array.iterator();
+		while (it.hasNext()) {
+			total = total + it.next();
+		}
+
+		return total;
+	}
+
+	public static void main(String[] args) {
+		// CompareArrayNumbers c= new CompareArrayNumbers();
+		List<Integer> a;
+		a = new ArrayList<>();
+
+		a.add(2);
+		a.add(4);
+		a.add(-1);
+		a.add(8);
+
+		System.out.println(SumArrayNumbers.sum(a));
+
+	}
 }
